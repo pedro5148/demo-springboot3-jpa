e1d2f67f283bf6dea7f77716c6d7b4d054750e63
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-24 13:47:34 -0300

    Criacao do projeto

---
+++ src/main/java/com/java/demospring3jpa/DemoSpring3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.java.demospring3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring3JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/demospring3jpa/DemoSpring3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.demospring3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpring3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
