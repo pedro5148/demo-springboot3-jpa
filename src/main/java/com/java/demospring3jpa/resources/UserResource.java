800592c2b99eb9cdafcec6a10cb00b0c4266d7ad
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-26 15:00:50 -0300

    Camada de service

--- src/main/java/com/java/demospring3jpa/resources/UserResource.java
+++ src/main/java/com/java/demospring3jpa/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.java.demospring3jpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.demospring3jpa.entities.User;
+import com.java.demospring3jpa.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Pedro Eugenio", "pedro@email.com", "9999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> lista = service.findAll(); 
+		return ResponseEntity.ok().body(lista);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
