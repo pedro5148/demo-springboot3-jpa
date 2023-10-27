e9cd2060514aab7be1f75fdec0fa5b04cbcb1d1d
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-27 17:22:24 -0300

    Excessao findbyid

--- src/main/java/com/java/demospring3jpa/services/UserService.java
+++ src/main/java/com/java/demospring3jpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.java.demospring3jpa.entities.User;
 import com.java.demospring3jpa.repositories.UserRepository;
+import com.java.demospring3jpa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
