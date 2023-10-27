cd267a3cdbd81c62599095928648f188ed3c7e7d
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-27 14:52:58 -0300

    Associacao orderitem, correcao

---
+++ src/main/java/com/java/demospring3jpa/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.java.demospring3jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.java.demospring3jpa.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
