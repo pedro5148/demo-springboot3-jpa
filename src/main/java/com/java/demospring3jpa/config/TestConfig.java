9a2eda93dea9e1fa4b82105408cc988d1e0dacc3
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-27 13:56:21 -0300

    Many to many joinTable

--- src/main/java/com/java/demospring3jpa/config/TestConfig.java
+++ src/main/java/com/java/demospring3jpa/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null, "Pedro Eugenio", "pedro@gmail.com", "988324534", "123456");
 		User u2 = new User(null, "Maria Paula", "maria@gmail.com", "97667679", "123456");
 		
