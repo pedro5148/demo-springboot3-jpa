bb9d914ac81a6dbba0e5927644859756d7ff466e
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-30 19:30:18 -0300

    Excessao update

--- src/main/java/com/java/demospring3jpa/services/UserService.java
+++ src/main/java/com/java/demospring3jpa/services/UserService.java
@@ -13,6 +13,8 @@ import com.java.demospring3jpa.repositories.UserRepository;
 import com.java.demospring3jpa.services.exceptions.DatabaseException;
 import com.java.demospring3jpa.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
