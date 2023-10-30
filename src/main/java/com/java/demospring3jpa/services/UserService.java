811b08306fb9ca3de136e41e49ebe27d2658b522
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-30 19:20:01 -0300

    Excessao delete

--- src/main/java/com/java/demospring3jpa/services/UserService.java
+++ src/main/java/com/java/demospring3jpa/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.java.demospring3jpa.entities.User;
 import com.java.demospring3jpa.repositories.UserRepository;
+import com.java.demospring3jpa.services.exceptions.DatabaseException;
 import com.java.demospring3jpa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
